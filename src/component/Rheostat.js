9fe0fa6ebdfc1c36fbc06f46b1eeef7a16469f88
Isaac Ayeni <isaac.ayeni@Hakeems-MacBook-Pro.local>
2023-10-13 10:24:10 +0100

    Updated and tested

--- src/component/Rheostat.js
+++ src/component/Rheostat.js
@@ -4,14 +4,15 @@ import Moment from "moment";
 import Rheostat, { AreaRheostat, BarRheostat } from "react-native-rheostat";
 
 const defaultProps = {
-  snapPoints: [
-    0, 60, 120, 180, 240, 300, 330, 360, 420, 480, 540, 570, 600, 630, 660, 690,
-    720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110,
-    1140, 1170, 1200, 1260, 1320, 1380, 1440,
-  ],
-  values: [480, 1040],
+  // snapPoints: [
+  //   0, 60, 120, 180, 240, 300, 330, 360, 420, 480, 540, 570, 600, 630, 660, 690,
+  //   720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110,
+  //   1140, 1170, 1200, 1260, 1320, 1380, 1440,
+  // ],
+
+  values: [100, 600],
   svgData: [
-    50, 50, 10, 10, 40, 40, 95, 95, 85, 85, 91, 35, 53, 53, 24, 50, 50, 10, 40,
+    50, 50, 20, 10, 40, 40, 95, 50, 85, 85, 91, 35, 53, 53, 24, 50, 50, 10, 40,
     95, 85, 91, 35, 53, 24, 50, 50, 10, 40, 95, 85, 91, 35, 53, 50, 50, 50, 10,
     40, 95, 91, 91, 24, 24, 50, 50, 10, 10,
   ],
@@ -37,7 +38,12 @@ export default class RheostatExample extends Component {
   onSliderDragEnd = () => {
     this.setState({ scrollEnabled: true });
   };
+
   render() {
+    const snapPoints = [];
+    for (let i = 0; i <= 2000; i += 50) {
+      snapPoints.push(i);
+    }
     return (
       <ScrollView
         contentContainerStyle={{ paddingHorizontal: 10 }}
@@ -47,7 +53,7 @@ export default class RheostatExample extends Component {
           <BarRheostat
             values={this.props.values}
             min={0}
-            max={1440}
+            max={2000}
             snap={true}
             snapPoints={this.props.snapPoints}
             svgData={this.props.svgData}
